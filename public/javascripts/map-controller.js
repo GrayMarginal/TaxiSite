fa104c224220f18cc70b13164cd62978d42fa4f7
GrayMarginal <blackofpixel@rabler.ru>
2018-05-19 16:46:23 +0600

    Добавлена форма отправки и скрытый фрейм

--- public/javascripts/map-controller.js
+++ public/javascripts/map-controller.js
@@ -2,6 +2,8 @@
 		var markers = [];
         var geocoder;
   var map;
+var timer = setInterval(function(){document.getElementById('map').style.height=document.documentElement.clientHeight+'px'; 
+document.getElementById('order-taxi').style.height=document.documentElement.clientHeight/100*30+'px';},1000);
   function initialize() {
     geocoder = new google.maps.Geocoder();
     var latlng = new google.maps.LatLng(54.9851249,73.3167058);
@@ -10,7 +12,8 @@
       center: latlng
     }
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
-  }
+
+ }
 
   function codeAddress() {
 	removeMarkers();
@@ -54,5 +57,16 @@ console.log("https://maps.googleapis.com/maps/api/directions/json?origin="+addre
     markers[i].setMap(null);
   }
   markers = [];}
+/*function createFrame(name, src, debug){
+src=src||'javascript:false';
+var tmpElem = document.createElement('div');
+tmpElem.innerHTML = '<iframe name="'+name+'" id="'+name+'" src="'+src+'">;
+var frame = tmpElem.firstChild;
+if(!debug){
+frame.style.display='none';
+}
+document.body.appendChild(frame);
+return frame;
+}*/
   //https://maps.googleapis.com/maps/api/directions/json?origin=address&destination=address2&key=AIzaSyCkgZHQMiEst3HhDiU_ejn9Miy4z6PplxA
   //"https://maps.googleapis.com/maps/api/directions/json?origin="+address.replace(" ", "+")+"&destination="+address2.replace(" ", "+")+"&key=AIzaSyCkgZHQMiEst3HhDiU_ejn9Miy4z6PplxA"   
