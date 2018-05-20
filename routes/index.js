55331423ff5dbe31198b71c325ecadc46c1f9378
GrayMarginal <blackofpixel@rabler.ru>
2018-05-20 15:18:05 +0600

    Добавлена страница 'О нас'

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.about = function(req, res){
+res.render('about', {title:'О нас', about:'Мы предоставляем качественные услуги в сфере такси'});
+};
