8f6792b65871be3ae48f8346b38fccc61b9133b2
Gray Marginal <blackofpixel@rambler.ru>
2018-06-09 09:08:29 +0600

    some changes

--- public/javascripts/form-controller.js
+++ public/javascripts/form-controller.js
@@ -2,9 +2,10 @@ $(document).ready(function(){
 	document.getElementById("arrivalTime").min = new Date().getHours()+':'+new Date().getMinutes();
 	document.getElementById("arrivalTime").value = new Date().getHours()+':'+new Date().getMinutes();
 var timeSetter = setInterval(function(){
-	//if (document.getElementById("arrivalTime").value < (new Date().getHours()+':'+new Date().getMinutes())){
-		document.getElementById("arrivalTime").min = new Date().getHours()+':'+new Date().getMinutes();
-		document.getElementById("arrivalTime").value = new Date().getHours()+':'+new Date().getMinutes();
-	//}
+	var time = document.getElementById("arrivalTime");
+	if(time.value<new Date().getHours()+':'+new Date().getMinutes()){
+		time.min = new Date().getHours()+':'+new Date().getMinutes();
+		time.value = new Date().getHours()+':'+new Date().getMinutes();
+		}
 }, 1000);
 })
