04d2d6a27e3c4441c713c06d6000d6c32f21ecfa
Gray Marginal <blackofpixel@rambler.ru>
2018-06-10 02:26:45 +0600

    repaired time

--- public/javascripts/form-controller.js
+++ public/javascripts/form-controller.js
@@ -1,11 +1,34 @@
 $(document).ready(function(){
-	document.getElementById("arrivalTime").min = new Date().getHours()+':'+new Date().getMinutes();
-	document.getElementById("arrivalTime").value = new Date().getHours()+':'+new Date().getMinutes();
-var timeSetter = setInterval(function(){
-	var time = document.getElementById("arrivalTime");
-	if(time.value<new Date().getHours()+':'+new Date().getMinutes()){
-		time.min = new Date().getHours()+':'+new Date().getMinutes();
-		time.value = new Date().getHours()+':'+new Date().getMinutes();
+	var curTime = {
+		hours:new Date().getHours().toString(),
+		minutes:new Date().getMinutes().toString(),
+		getFullTime:function(){
+			var h, m;
+			if(Number(this.hours)<=9){
+				h = "0"+this.hours;
+			}else{
+				h = this.hours;
+			}
+			if(Number(this.minutes)<=9){
+				m = "0"+this.minutes;
+			}else{
+				m = this.minutes;
+			}
+			return h+":"+m;
+		},
+		update:function(){
+			this.hours = new Date().getHours().toString();
+			this.minutes = new Date().getMinutes().toString();
 		}
-}, 1000);
+	}
+	//document.getElementById("arrivalTime").min = new Date().getHours()+':'+new Date().getMinutes();
+	document.getElementById("arrivalTime").value = new Date().getHours()+':'+new Date().getMinutes();
+setInterval(function(){
+	var time = document.getElementById("arrivalTime");
+	curTime.update();
+	if(time.value<curTime.getFullTime()){
+		time.min = curTime.getFullTime();
+		time.value = curTime.getFullTime();
+	}
+		}, 1000);
 })
