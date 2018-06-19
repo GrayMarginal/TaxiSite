9bdffcf0a69a2c5460388f6f4e3c016ac6d0e6ce
Коновалова Светлана <sweetochka1997@gmail.com>
2018-06-19 21:50:01 +0600

    --

--- routes/driver.js
+++ routes/driver.js
@@ -97,5 +97,20 @@ exports.answer = function(req, res){
 		});
 		});
     });
+	}else if(req.body.orderComplete){
+		sql.open(conString, function(err, con){
+        if(err){
+            console.log('failed to open '+err.message);
+            return;
+        }
+        con.query( "update Orders set State ='Завершен' where ID_Order="+req.body.ID_Order, function (err, rows) {
+          if (err) {
+            console.log(err.message);
+            return;
+          }
+			res.set("Access-Control-Allow-Origin","*");
+          res.send({status:"OK"})
+		});
+		});
 	}
 }
